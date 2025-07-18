4817e5b4db31c490c51c5c7744f147c0fc04771d
engahmed309 <enga.abdelwahed@gmail.com>
2025-07-18 20:03:13 +0300

    use new token (my token)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
 import 'package:uuid/uuid.dart';
 
-const userId = "Plo_Koon";
+const userId = "ahmed123";
 final String userName = const Uuid().v4();
 const userToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1Bsb19Lb29uIiwidXNlcl9pZCI6IlBsb19Lb29uIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3NTI4MDY0NzYsImV4cCI6MTc1MzQxMTI3Nn0.f-05Fb2JsOZnCW6oEupcRku6nzUaoK_jgOlr55cEsu8';
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWhtZWQxMjMiLCJleHAiOjE3MjE0NjAwMDB9.h3-jqBhW8ZORcL2v6cgbjU4G62IK9bXYRvwvwec5Tj0";
 const apiKey = "nh2p3jftac2x";
 const callId = "hKdfTFLBufDl";
